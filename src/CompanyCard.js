91955bd9df688cdc7e3e35d1dc5c66104389067a
Russell Jones <justyrones@gmail.com>
2023-05-31 15:12:24 -0600

    making changes

--- src/CompanyCard.js
+++ src/CompanyCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './CompanyCard.css'
 
  /** DESCRIPTION
 *
@@ -13,14 +14,14 @@ import { Link } from "react-router-dom";
 function CompanyCard ({ company }) {
 
   return (
-    <div className="container my-5 mx-auto text-left">
+    <div className="container my-5 mx-auto text-left company-card">
       <Link
         to={`companies/:name`}
-        className="text-center text-dark company-card"
+        className="text-left text-dark"
         style={{ textDecoration: 'none' }}
       >
-        <h1 className="company-title">{company.name}</h1>
-        <p>{company.description}</p>
+        <h1 className="company-title text-dark">{company.name}</h1>
+        <p className="text-dark">{company.description}</p>
       </Link>
     </div>
   );
