d87532a876fb70b0040070261d619e78a9e8d02a
Crynowecer0 <crynowecer11@gmail.com>
2023-05-31 23:00:50 -0400

    refactor Jobs per code-review comments, add TODOs for new components

--- src/Jobs.js
+++ src/Jobs.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // import { useParams } from 'react-router-dom';
-import SearchForm from './SearchForm';
-import JobCardList from './JobCardList';
-import JoblyApi from './api';
+import SearchForm from "./SearchForm";
+import JobCardList from "./JobCardList";
+import JoblyApi from "./api";
 
- /** DESCRIPTION
+/** DESCRIPTION
 *Jobs uses useState and useEffect to make an AJAX request for all the jobs and
 saves them in state then passes the jobs to JobCardList and passes the function
  to change the state of jobs to the SearchForm.
@@ -16,51 +16,62 @@ saves them in state then passes the jobs to JobCardList and passes the function
 *
 * RoutesList -> Jobs -> {SearchForm, JobCardList}
 */
-function Jobs () {
+function Jobs() {
   const [allJobs, setAllJobs] = useState({
     isLoading: true,
-    jobs: []
+    jobs: [],
   });
+
+  const [errors, setErrors] = useState({
+    hasError: false,
+    errorMessage: "",
+  });
+
   /** Gets all the jobs via AJAX request then saves them to state*/
   useEffect(() => {
-    async function getAllJobs(){
+    async function getAllJobs() {
       // jobs > res
-      const res = await JoblyApi.getJobs();
+      const jobs = await JoblyApi.getJobs();
       setAllJobs({
         isLoading: false,
-        jobs: res
-      })
+        jobs: jobs,
+      });
     }
     getAllJobs();
-  }, [])
+  }, []);
 
   /**  submitSearch receives a string and makes an AJAX request via our JoblyApi
    *  class and saves the results to state or alerts with error.*/
   async function submitSearch(params) {
-    console.log('params in submitSearch', params)
+    console.log("params in submitSearch", params);
     try {
-            // jobs > res
-      const res = await JoblyApi.getJobs(params);
+      // jobs > res
+      const jobs = await JoblyApi.getJobs(params);
       setAllJobs({
         isLoading: false,
-        jobs: res,
+        jobs: jobs,
       });
     } catch (err) {
-      // alert  < component
-      window.alert("there was an error with your search");
+      setErrors({
+        hasError: true,
+        errorMessage: err.message,
+      });
     }
   }
 
-  if(allJobs.isLoading) return <h1> Loading...</h1>
+  //TODO: loading screen component
+  if (allJobs.isLoading) return <h1> Loading...</h1>;
+  //TODO: error screen component
+  if (errors.errorMessage) return <h1>error: {errors.errorMessage}</h1>;
 
   return (
     <div>
       <div>
-        <SearchForm submitSearch={submitSearch}/>
-        <JobCardList jobs={allJobs.jobs}/>
+        <SearchForm submitSearch={submitSearch} />
+        <JobCardList jobs={allJobs.jobs} />
       </div>
     </div>
-  )
+  );
 }
 
 export default Jobs;
